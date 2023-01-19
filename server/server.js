4736c8a7fd72ca9ed7bc9a33e46c3f5152ac2476
Saamahn Mahjouri <mahjouri.saamahn@gmail.com>
2023-01-19 13:41:17 -0500

    update the server url

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const configuration = new Configuration({
-    organization: process.env.OPENAI_ORG,
     apiKey: process.env.OPENAI_API_KEY,
 });
 
